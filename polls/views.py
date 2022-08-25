0aacb83cafa04ff6a22d8c2c0639082d0174fe1a
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-25 05:16:27 -0500

    Def detail

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-# Create your views here.
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
